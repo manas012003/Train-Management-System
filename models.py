e178c41919b77059353ad5a1135b6488f1559dc1
manas012003 <99064841+manas012003@users.noreply.github.com>
2024-05-17 22:48:20 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def __init__(self, username, password, is_admin=False):
+        self.username = username
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+        self.is_admin = is_admin
+
+class Train(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    source = db.Column(db.String(80), nullable=False)
+    destination = db.Column(db.String(80), nullable=False)
+    total_seats = db.Column(db.Integer, nullable=False)
+    available_seats = db.Column(db.Integer, nullable=False)
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    train_id = db.Column(db.Integer, db.ForeignKey('train.id'), nullable=False)
+    seats_booked = db.Column(db.Integer, nullable=False)
+    user = db.relationship('User', backref='bookings', lazy=True)
+    train = db.relationship('Train', backref='bookings', lazy=True)
